152083cf632bb47c5c30d0bfb9207eba744f0ac1
Jhenny Cervera <jhenny,cervera@pucp.edu.pe>
2024-08-30 21:26:26 -0500

    add frontend

--- sql/data.sql
+++ sql/data.sql
@@ -1,4 +1,12 @@
 insert into public."Books"("title", "author", "foundation_date") 
 values 
-    ('facebook', 'Mark Zuckerrberg', '2004-02-04'),
-    ('title', 'author', '2004-03-04');
+    ('To Kill a Mockingbird', 'Harper Lee', '1960-07-11'),
+    ('1984', 'George Orwell', '1949-06-08'),
+    ('Pride and Prejudice', 'Jane Austen', '1813-01-28'),
+    ('The Great Gatsby', 'F. Scott Fitzgerald', '1925-04-10'),
+    ('Moby-Dick', 'Herman Melville', '1851-10-18'),
+    ('The Catcher in the Rye', 'J.D. Salinger', '1951-07-16'),
+    ('The Hobbit', 'J.R.R. Tolkien', '1937-09-21'),
+    ('War and Peace', 'Leo Tolstoy', '1869-01-01'),
+    ('Crime and Punishment', 'Fyodor Dostoevsky', '1866-01-01'),
+    ('Brave New World', 'Aldous Huxley', '1932-08-18');
